479a9894bb911503f71d0b0625a6ff16f49d57ce
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-09-29 12:29:25 +0530

    py

---
+++ Python_Notes/Day1/Ex1.py
@@ -0,0 +1,35 @@
+
+#  What is Python?
+# • Python is a high-level language like other high-level language such
+# as Java, C++, PHP, Ruby, Basic and Perl. •
+# Python is an object-oriented programming language. • Python provides security.
+# • The CPU understands a language which is called as Machine Language.
+
+# 1).Low level programming ---->NO need to compiler  --->Binay 0's ans 1's
+# 2).Intermediate level -----> Pseduco c,c++
+# 3).High-level programming --->Java, C++, PHP, Ruby, Basic and Perl
+
+
+# Applications of Python
+# We can use Python everywhere because Python is known for its general-purpose nature,
+# which makes it applicable in almost every domain of software development. The most common application areas are:
+#
+# Desktop Applications
+# Web Applications
+# Database Applications
+# Network Programming
+# Games
+# Data Analysis Applications
+# Machine Learning
+# Artificial Intelligence Applications
+# IoT
+
+# It was created by 'Guido van Rossum' , it was released in 1991.
+# MONTY'S PYTHON FLYING CIRCUS.
+
+# Interpreater: Interpreate is a soft ware, it run the code line by line
+# It having any errors it will through at runtime
+# If there is no errors then enter programming excutes at a time.
+
+
+
---
+++ Python_Notes/Day1/ex2.py
@@ -0,0 +1,26 @@
+# Comments:
+# The comments are description that help programmers to understanding the functionally of programm.
+# The comments are neccessary while writing ocde in a python (#) use the hash
+
+# Comments can be used to explain Python code.
+#
+# Comments can be used to make the code more readable.
+#
+# Comments can be used to prevent execution when testing code.  (cntrl + ?)
+
+print('Welcome to python')
+
+print('Welcome to GRK')
+#  IndentationError: unexpected indent
+
+# Welcome to python
+# Welcome to GRK
+
+# Python Indentation
+# Indentation refers to the spaces at the beginning of a code line.
+#
+# Where in other programming languages the indentation in code is for readability only,
+# the indentation in Python is very important.
+#
+# Python uses indentation to indicate a block of code.
+
