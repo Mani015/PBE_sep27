9c8d77926436c562922e7768908f081f03f62992
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-10-02 12:31:00 +0530

    py

---
+++ Python_Notes/Day03/ex1.py
@@ -0,0 +1,25 @@
+# Binary language:
+# If you work on binary number in python ,denoted by prefix "0b"
+
+a = 12
+# 12 is a integer
+# In this integer convert into a binary by using bin(number)
+# syn: bin(number)
+
+print(bin(a))
+# 0b1100
+
+print(bin(56))
+# 0b111000
+
+print(bin(44))
+# 0b101100
+print()
+print(bin(96))
+print(bin(77))
+print(bin(37))
+print(bin(27))
+# 0b1100000
+# 0b1001101
+# 0b100101
+# 0b11011
---
+++ Python_Notes/Day03/ex2.py
@@ -0,0 +1,12 @@
+
+# In the binary's only taken 0's and 1's
+# Conversion of binary to number:
+x = 0b1100
+print(x)
+
+y = 0b10101
+print(y)
+# 21
+z = 0b110011
+print(z)
+
