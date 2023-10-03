b833b4a85c5bacc89e730604cd4220cf43685a27
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-10-03 15:53:30 +0530

    py

--- Python_Notes/Day03/ex3.py
+++ Python_Notes/Day03/ex3.py
@@ -5,20 +5,23 @@
 
 # hexdigits)
 # 0123456789abcdefABCDEF
-
-x1 = 0x12a
-print(x1)
-# 298
-
-x2 = 0x123abc
-print(x2)
-# 1194684
-
-x3 = 0x47c8
-print(x3)
-# 18376
-
-x4 = 0x321b
-print(x4)
+#
+# x1 = 0x12a
+# print(x1)
+# # 298
+#
+# x2 = 0x123abc
+# print(x2)
+# # 1194684
+#
+# x3 = 0x47c8
+# print(x3)
+# # 18376
+#
+# x4 = 0x321b
+# print(x4)
 # 12827
+
+x5 = 0xa52
+print(x5)
 
---
+++ Python_Notes/Day03/ex4.py
@@ -0,0 +1,24 @@
+
+# It is denoted by 0o
+# octdigits
+# 01234567
+
+# Number to octal decimal value
+# syn: oct(number)
+x = 13
+print(oct(x))
+# 0o15
+
+y = 123
+print(oct(y))
+# 173
+
+
+# Octal to number
+a1 = 0o123
+print(a1)
+# 83
+
+a2 = 0o2233
+print(a2)
+# 1179
