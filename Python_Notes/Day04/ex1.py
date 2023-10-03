33f1f49ad8288be405b0609f67bd0d5d0ca13c30
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-10-03 15:52:45 +0530

    py

---
+++ Python_Notes/Day04/ex1.py
@@ -0,0 +1,42 @@
+
+# Python divides the operators in the following groups:
+#
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+# Arithmetic operators
+# ' + '---->plus
+
+a = 10
+b = 12
+# syntax: operand1 + operand2
+
+print(a + b)
+# 22
+
+# ' - ' --> subtraction
+print(a-b)
+# -2
+
+# ' * ' ---> Asterik
+print(b*a)
+# 120
+
+print(a*2*b)
+# 240
+
+# ' / '--> Division
+# Always taking floating value
+
+print(20/10)
+# 2.0
+
+print(30/5)
+# 6.0
+
+
---
+++ Python_Notes/Day04/ex2.py
@@ -0,0 +1,45 @@
+
+# '//' -->Floor Division
+# Ignore the floating value, take's  a digit
+print(30//5)
+# 6
+
+print(20//2)
+# 10
+
+
+# ' ** ' -->Exponentation --->power
+
+print(2**2)
+# 4
+print(3**2)
+# 9
+
+print(25**3)
+# 25 * 25 * 25
+#  625 * 25
+# 15625
+
+# ' % '---> Modul's
+
+a = 20
+b = 5
+print(a % b)
+# 0
+print(5%2)
+
+# 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
